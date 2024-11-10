29885887920a155345a6e461e279a10e90db253d
Abid Hussain <61841766+abid-glitch@users.noreply.github.com>
2024-11-11 03:24:14 +0530

    Create Java Stdin and Stdout II.java

---
+++ Hacckerrank/Java Stdin and Stdout II.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Solution{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Double d = sc.nextDouble();
+        sc.nextLine();
+        String s = sc.nextLine();
+        
+        System.out.println("String: "+ s);
+
+        System.out.println("Double: "+ d);
+
+        System.out.println("Int: "+ n);
+        
+    }
+}
