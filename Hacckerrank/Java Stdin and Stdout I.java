78a93442cdacec919ce12ca3ae074b3889694a40
Abid Hussain <61841766+abid-glitch@users.noreply.github.com>
2024-11-11 03:10:28 +0530

    Create  Java Stdin and Stdout I.java

---
+++ Hacckerrank/Java Stdin and Stdout I.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=0;
+        for(int i=0;i<3;i++){
+            n= sc.nextInt();
+            System.out.print(n);
+            System.out.println();
+
+
+        }
+        
+    }
+}
