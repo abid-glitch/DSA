ae6e255945ea57e040916fc0b9c2fb68d7079ba2
Abid Hussain <61841766+abid-glitch@users.noreply.github.com>
2025-01-17 14:30:25 +0530

    Update PrimeFactors.java

--- GFG_DSA/Mathematics/PrimeFactors.java
+++ GFG_DSA/Mathematics/PrimeFactors.java
@@ -32,4 +32,32 @@ public class PrimeFactors {
         if(input > 1)
             System.out.println(input);
     }
+
+        // More Optimized Version
+    // Time Complexity - O(n^(1/2))
+    // Space and Aux - O(1)
+    static void primefactor(int input){
+        if(input<=1)
+            System.out.println("Don't have any primefactors");
+        while(input%2==0) {
+            System.out.println("2");
+            input = input/2;
+        }
+        while(input%3==0){
+            System.out.println("3");
+            input = input/3;
+        }
+        for(int i=5;i*i<=input;i=i+6){
+            while(input%i==0){
+                System.out.println(i);
+                input = input/i;
+            }
+            while(input%(i+2)==0){
+                System.out.println(i+2);
+                input = input/(i+2);
+            }
+        }
+        if(input > 3)
+            System.out.println(input);
+    }
 }
