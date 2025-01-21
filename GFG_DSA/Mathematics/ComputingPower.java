4175c12d239887be481a2b4fad7621d476a3e2f3
Abid Hussain <61841766+abid-glitch@users.noreply.github.com>
2025-01-21 09:53:24 +0530

    Update ComputingPower.java

--- GFG_DSA/Mathematics/ComputingPower.java
+++ GFG_DSA/Mathematics/ComputingPower.java
@@ -14,26 +14,43 @@ public class computingPower {
     // Brute Force Approach
     // Time Complexity - O(n)
     // Space and Aux - O(1)
-    static int compute(int input, int power){
-        int result = 1;
-        for(int i=1;i<=power;i++){
-            result = result * power;
-        }
-        return result;
-    }
+   static int compute(int input, int power){
+       int result = 1;
+       for(int i=1;i<=power;i++){
+           result = result * power;
+       }
+       return result;
+   }
 
-    // Optimized Approach 
+    // Optimized Approach
     // Time Complexity - O(log(n))
     // Space and Aux - O(log(n)) - Recursive call stack
+   static int compute(int input, int power){
+       if(power==0)
+           return 1;
+       int temp = compute(input, power/2);
+       temp = temp * temp;
+       if(power%2 == 0)
+           return temp;
+       else
+           return temp * input;
+   }
+
+    // Iterative Approach
+    // Time Complexity - O(log(n))
+    // Space and Aux - O(1)
     static int compute(int input, int power){
-        if(power==0)
-            return 1;
-        int temp = compute(input, power/2);
-        temp = temp * temp;
-        if(power%2 == 0)
-            return temp;
-        else
-            return temp * input;
+        int result = 1;
+        while(power > 0){
+            if(power % 2!=0){
+                result = result * input;
+            }
+            input  = input * input;
+            power = power/2;
+
+
+        }
+        return result;
     }
 
 }
